c56fd6d7902c21298ca51c664e237d282ca7728e
Maribel Martos <mmartosar@uoc.edu>
2026-05-24 16:58:36 +0200

    Completajo ejercicio de intersecion del tema 4

--- 4a/ej4a1.py
+++ 4a/ej4a1.py
@@ -1,3 +1,5 @@
+
+
 """
 Enunciado:
 Dadas dos listas de elementos, implementa una función llamada
@@ -38,12 +40,15 @@ list_2 = [4, 5, 6, 7, 8]
 
 
 def find_intersection(list_1, list_2):
-    # Write here your code
-    pass
+    intersection = []
+    for e in list_1:
+        if e in list_2 and e not in intersection:
+            intersection.append(e)
+    return intersection 
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
-# print(find_intersection([1, 2, 3, 4], [3, 4, 5, 6]))
-# print(find_intersection(['apple', 'banana', 'orange'], ['banana', 'kiwi', 'apple']))
+print(find_intersection([1, 2, 3, 4], [3, 4, 5, 6]))
+print(find_intersection(['apple', 'banana', 'orange'], ['banana', 'kiwi', 'apple']))
