4a683888ad2a1fdeb005eb8cc998bd2047bded94
Maribel Martos <mmartosar@uoc.edu>
2026-05-24 17:29:44 +0200

    Ejercicio de creacion de clases del tema 4c

--- 4c/ej4c1.py
+++ 4c/ej4c1.py
@@ -92,7 +92,8 @@ class Person:
 
     # Method that returns the description of the person
     def describe(self):
-        # Write here your code
+        person_1_description = f"{self.name} is {self.age} years old."  
+        return person_1_description 
         pass
 
 
@@ -100,12 +101,15 @@ class Person:
 class Student(Person):
     # Constructor
     def __init__(self, name, age, major):
-        # Write here your code
+        super().__init__(name, age)
+        self.major = major  
         pass
 
     # Method that returns the description of the student
     def describe(self):
-        # Write here your code
+        description = super().describe()
+        student_1_description = f"{description} Studies {self.major}."    
+        return student_1_description  
         pass
 
 
@@ -116,5 +120,5 @@ student_1 = Student("Ana", 25, "Systems Engineering")
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
 # Access to object methods
-#print(person_1.describe())  # Juan is 30 years old.
-#print(student_1.describe())  # Ana is 25 years old. Studies Systems Engineering.
+print(person_1.describe())  # Juan is 30 years old.
+print(student_1.describe())  # Ana is 25 years old. Studies Systems Engineering.
